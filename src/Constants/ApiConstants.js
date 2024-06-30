2007244221b3b74adaf987190486e57cf2e72416
yagya-narayan-tiwari <161701321+yagya-narayan-tiwari@users.noreply.github.com>
2024-06-30 14:21:53 +0530

    Update ApiConstants.js

--- src/Constants/ApiConstants.js
+++ src/Constants/ApiConstants.js
@@ -1,9 +1,9 @@
-// export const API_BASE_URL = 'http://127.0.0.1:6800';
-// export const SAVE_STUDENT_API_ROUTE = `${API_BASE_URL}/students`;
-// export const GET_STUDENT_API_ROUTE = SAVE_STUDENT_API_ROUTE;
-// export const REMOVE_STUDENT_API_ROUTE = SAVE_STUDENT_API_ROUTE;
-// export const UPDATE_STUDENT_API_ROUTE = SAVE_STUDENT_API_ROUTE;
-// export const ADMIN_LOGIN_URL = `${API_BASE_URL}/admins/login`
+ export const API_BASE_URL = 'https://shine-library-api.vercel.app';
+ export const SAVE_STUDENT_API_ROUTE = `${API_BASE_URL}/students`;
+ export const GET_STUDENT_API_ROUTE = SAVE_STUDENT_API_ROUTE;
+ export const REMOVE_STUDENT_API_ROUTE = SAVE_STUDENT_API_ROUTE;
+export const UPDATE_STUDENT_API_ROUTE = SAVE_STUDENT_API_ROUTE;
+ export const ADMIN_LOGIN_URL = `${API_BASE_URL}/admins/login`
 
 
 // Quote reated api call
